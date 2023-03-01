84f13e8ff914b8d8660009b2f39b036b3632b930
ahmedrazakhan-412 <ahmedrazakhan412@gmail.com>
2023-03-01 13:31:30 +0530

    On 1st March 2023

---
+++ 1stMockQusetions/AdditionOfNumbersInString.js
@@ -0,0 +1,31 @@
+/* Seperate all characters in string like UpperCase, LowerCase, Numbers, SpecailCharacters
+Input - AxB5#*eZ5
+Output -  10
+*/
+  function seperateAll(arr) {
+  var Numbers = "";
+		for (let index = 0; index < arr.length; index++) {
+       if(arr[index]>=0 && arr[index]<=9) {
+        Numbers = Numbers+arr[index]
+      }
+      else{
+        Numbers = Numbers+"*";
+      } 
+    }
+    console.log(Numbers);
+    var arrnum = Numbers.split("*");
+    console.log(arrnum);
+    var num = 0;
+    var total = 0;
+    for (let index = 0; index < arrnum.length; index++) {
+      if (arrnum[index]=="") {
+      } 
+      else{
+      num = Number(arrnum[index]);
+      total = total+num;
+      }
+    }
+    console.log("The Addition of All Numbers are :"+total);
+  }
+  seperateAll("AxB5#*eZ45jhjhh40gcg2000"); 
+    
